5fefdbc414eb57625108cdf7d8504e36da197405
jyork <jyork@dwolla.com>
2019-11-02 19:42:35 -0500

    exercise 2.4 UnCurry

---
+++ src/main/scala/fpinscala/gettingstarted/UnCurry.scala
@@ -0,0 +1,9 @@
+//Implement Uncurry, which reverses the transformation of curry
+package gettingstarted
+
+object Uncurry {
+
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C =
+    (a: A, b: B) => f(a)(b) //f(a)(b), f(a) returns a function
+
+}
