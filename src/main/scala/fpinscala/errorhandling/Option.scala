0615e3da329dc360110551bf30d07201731daf1e
Jen York <jyork@dwolla.com>
2020-02-14 15:54:52 -0600

    complete 4.1

--- src/main/scala/fpinscala/errorhandling/Option.scala
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -6,10 +6,19 @@ sealed trait Option[+A]{
     case Some(a) => Some(f(a))
     case None => None
   }
-  def flatMap[B](f: A => Option[B]): Option[B] = ??? //Apply f, which may fail, to the Option if not None
-  def getOrElse[B >: A](default: => B): B = ??? //The B >: A says that the B type parameter must be a supertype of A
-  def orElse[B >: A](ob: Option[B]): Option[B] = ??? //Don't evaluate ob unless needed
-  def filter(f: A => Boolean): Option[A] = ??? //Convert Some to None if the value doesn't satisfy f.
+  def flatMap[B](f: A => Option[B]): Option[B] = { //Apply f, which may fail, to the Option if not None
+    map(f) getOrElse None
+  }
+  def getOrElse[B >: A](default: => B): B = this match { //The B >: A says that the B type parameter must be a supertype of A
+    case Some(a) => a
+    case None => default
+  }
+  def orElse[B >: A](ob: Option[B]): Option[B] = { //Don't evaluate ob unless needed
+    map(Some(_)) getOrElse ob
+  }
+  def filter(f: A => Boolean): Option[A] = { //Convert Some to None if the value doesn't satisfy f.
+    flatMap((a: A) => if(f(a)) Some(a) else None)
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
