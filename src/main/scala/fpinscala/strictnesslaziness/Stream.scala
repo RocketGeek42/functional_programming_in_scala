be1df69e21de04f4565dc98c8bd765cfcadefa37
Jen York <jyork@dwolla.com>
2020-04-03 13:09:37 -0500

    add base for 5.3 through 5.6

--- src/main/scala/fpinscala/strictnesslaziness/Stream.scala
+++ src/main/scala/fpinscala/strictnesslaziness/Stream.scala
@@ -8,6 +8,17 @@ sealed trait Stream[+A] {
   def take(n: Int): Stream[A] = ???
   def drop(n: Int): Stream[A] = ???
 
+  //5.3 Write the function takeWhile for returning all starting elements of a Stream that match the given predicate.
+  def takeWhile(p: A => Boolean): Stream[A] = ???
+
+  //5.4 Implement forAll, which checks that all elements in the Stream match a given predicate.  Your implementation should terminate the traversal as soon as it encounters a nonmatching value.
+  def forAll(p: A => Boolean): Boolean = ???
+
+  //5.5 Use foldRight to implement takeWhile
+
+
+  //5.6 Implement headOption using foldRight.
+
 
 }
 case object Empty extends Stream[Nothing]
