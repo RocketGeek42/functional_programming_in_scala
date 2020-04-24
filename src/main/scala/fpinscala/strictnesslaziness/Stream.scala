59d19c798778ae9416abed5e75bfcba1dedd63fa
Jen York <jyork@dwolla.com>
2020-04-24 14:53:58 -0500

    optimizations round 1

--- src/main/scala/fpinscala/strictnesslaziness/Stream.scala
+++ src/main/scala/fpinscala/strictnesslaziness/Stream.scala
@@ -1,18 +1,4 @@
-sealed trait Stream[+A]
-case object Empty extends Stream[Nothing]
-case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
-
-object Stream {
-  def cons[A] (hd: => A, tl: => Stream[A]): Stream[A] = {
-    lazy val head = hd
-    lazy val tail = tl
-    Cons(() => head, () => tail)
-  }
-  def empty[A]: Stream[A] = Empty
-
-  def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
-
+sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
@@ -31,12 +17,26 @@ object Stream {
     case _ => Empty
   }
 
-  @annotation.tailrec
   def drop(n: Int): Stream[A] = this match {
     case Empty => Empty
     case Cons(_, t) if n > 0 => t().drop(n - 1)
     case Cons(_, _) if n <= 0 => this
   }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A] (hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
 
   //5.3 Write the function takeWhile for returning all starting elements of a Stream that match the given predicate
   def takeWhile(p: A => Boolean): Stream[A] = this match {
